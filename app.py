fb93d2b0182cceed58dbd76da4cc8ffdb59b1d4e
ROSTIG-NOX3N <137249382+ROSTIG-NOX3N@users.noreply.github.com>
2025-04-24 01:30:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 import streamlit as st
-from team_data import load_team_data
-from ui_display import display_dashboard
+from ui_display import display_groups
 
-# 페이지 설정
-st.set_page_config(page_title="아침체인지컵 2025 - 대시보드", layout="wide")
+# 각 조별 팀 목록
+groups = {
+    "A": ["1-2", "1-1", "1-4"],
+    "B": ["2-6", "2-7", "3-7"],
+    "C": ["2-2", "1-7", "2-5"],
+    "D": ["2-3", "3-2", "3-6"],
+    "E": ["3-4", "1-6", "1-3"],
+    "F": ["3-3", "3-5", "3-1"],
+    "G": ["1-5", "2-1", "2-4"],
+}
 
-# 팀 데이터 파일 경로
-file_path = 'group_data.csv'
+# 제목 및 페이지 설정
+st.set_page_config(page_title="아침체인지컵 2025 - 조별 팀 구성", layout="wide")
+st.title("⚽ 아침체인지컵 2025 - 조별 팀 구성")
 
-# 팀 데이터 불러오기
-groups = load_team_data(file_path)
-
-# 대시보드 표시
-display_dashboard(groups)
+# 그룹 표시
+display_groups(groups)
