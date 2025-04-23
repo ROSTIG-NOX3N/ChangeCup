7851c29a51fbd67ff530145dd36a16549ded79b2
ROSTIG-NOX3N <137249382+ROSTIG-NOX3N@users.noreply.github.com>
2025-04-24 01:28:08 +0900

    Create match_results.py

---
+++ match_results.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def display_results():
+    # 경기 결과 예시
+    st.subheader("경기 결과")
+    st.write("A조 1번경기: 1-2 결과")
+    st.write("A조 2번경기: 1-1 결과")
+    st.write("B조 1번경기: 2-6 결과")
+    st.write("...")
