7d1685285bd0bf91824b8b183d36edb5212a802c
ROSTIG-NOX3N <137249382+ROSTIG-NOX3N@users.noreply.github.com>
2025-04-24 19:14:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -112,14 +112,6 @@ elif option == "반별 통계":
     # 각 반의 성적을 분석하여 추가 컬럼 생성 (승률, 실점 등)
     class_stats_df['승률'] = class_stats_df['승'] / (class_stats_df['승'] + class_stats_df['무'] + class_stats_df['패'])
     
-    # 색상 강조: 승률이 높은 반을 초록색, 낮은 반을 빨간색으로 강조
-    def highlight_best_worst(row):
-        if row['승률'] >= 0.6:
-            return ['background-color: green'] * len(row)
-        elif row['승률'] <= 0.3:
-            return ['background-color: red'] * len(row)
-        return [''] * len(row)
-
     # 데이터프레임을 스타일링하여 승률 기준으로 색상 추가
     styled_df = class_stats_df.style.apply(highlight_best_worst, axis=1)
 
